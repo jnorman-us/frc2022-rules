6f9ed209aabdd8a1f2c57270e7f33c5a0d5d18bc
josephnormandev <jnormantransactions@gmail.com>
2022-03-02 22:04:55 -0600

    adds a read me, adds the human interaction rules

--- index.js
+++ index.js
@@ -17,17 +17,10 @@ const theme_question = chalk.bold.cyan;
 const theme_correct = chalk.bold.green;
 const theme_wrong = chalk.bold.red;
 const theme_perfect = chalk.inverse.yellow;
-const answerLetters = [0, 1, 2, 3];		
 
-function askQuestion(code, name, description, answers) {
+function askQuestion(code, name, description) {
 	console.log(theme_code('Rule ' + code) + theme_answer(' - ') + theme_name(name));
 	console.log(theme_description(description));
-
-	let i = 0;
-	for(const answer of answers) {
-		console.log(theme_answerLetter(answerLetters[i] + ')') + theme_answer(' ' + answer));
-		i ++;
-	}
 
 	return prompt(theme_question('Answer: '));
 }
@@ -45,6 +38,7 @@ for(const index in rules) {
 
 // ask the questions
 let correctN = 0;
+let reviewRules = [];
 while(indexes.length > 0) {
 	console.log('\n');
 	console.log(theme_progress(`${(ruleN - indexes.length + 1)} / ${ruleN}`));
@@ -57,33 +51,27 @@ while(indexes.length > 0) {
 	const code = question.code;
 	const name = question.name;
 	const description = question.description;
-	const answer = question.violation;
+	const answerArray = question.violation;
+	answerArray.sort();
 
-	const randomAnswers = [];
-	for(let i = 0; i < 3; i ++) {
-		const randomIndex = Math.floor(Math.random() * rules.length);
-		const randomRule = rules[randomIndex];
-		const randomAnswer = randomRule.violation;
+	const userInput = askQuestion(code, name, description);
 
-		if(answer == randomAnswer || randomAnswers.includes(randomAnswer)) {
-			i --;
-			continue;
-		}
-
-		randomAnswers.push(randomAnswer);
+	const userInputArray = userInput.split(',');
+	for(let i = 0; i < userInputArray.length; i ++) {
+		let text = userInputArray[i];
+		text = text.trim();
+		text = text.toUpperCase();
+		userInputArray[i] = text;
 	}
+	userInputArray.sort();
 
-	const correctIndex = Math.floor(Math.random() * 4);
-	randomAnswers.splice(correctIndex, 0, answer);
-
-	const userInput = askQuestion(code, name, description, randomAnswers);
-
-	if(userInput == correctIndex) {
+	if(userInputArray.join() == answerArray.join()) {
 		console.log(theme_correct('✓ Correct!'));
 		correctN ++;
 	} else {
+		reviewRules.push(code); 
 		console.log(theme_wrong('✖ Incorrect!'));
-		console.log(theme_answer(`The correct answer is "${answer}"`))
+		console.log(theme_answer(`The correct answer is "${answerArray.join()}"`))
 	}
 }
 
@@ -91,4 +79,5 @@ if(correctN == ruleN) {
 	console.log(theme_perfect('☆☆☆ You scored 100% ☆☆☆'));
 } else {
 	console.log(theme_wrong(`✖✖✖ You scored ${correctN}/${ruleN}✖✖✖`));
+	console.log(theme_answer(`Review these rules: ${ reviewRules.join()}`));
 }
---
+++ readme.md
@@ -0,0 +1,22 @@
+# CLI 2022 FRC Game Quiz
+I wrote this project to prepare myself for the FIT Waco Districts, where I will be serving as referee. Wish me luck.
+
+## Explanation
+The questions that are asked pertain to match play and competition. None of the rules related to robot design are included. The quiz presents the rule and asks you to recall the penalties associated with breaking the rules.
+
+	42 / 59
+	Rule G403 - 2 cargo max
+	Robot may not have greater than momentary control of 3 cargo at a time
+	Answer: foul/cargo
+	✖ Incorrect!
+	The correct answer is "FOUL/CARGO,YELLOW CARD"
+
+## Usage
+If you want to use this program to teach yourself the rules, clone this repo and run the following commands
+
+	git clone git@github.com:josephnormandev/frc2022-rules.git
+	cd frc2022-rules
+	npm i
+	npm start
+
+From there you will be asked to complete the 59 question exam. Upon completion, the program will tell you which rules to review (you got wrong). It will be fairly difficult starting off, but I'm sure repeatedly taking this quiz will get you to 100%. If you want to reference the rules, visit [here](https://firstfrc.blob.core.windows.net/frc2022/Manual/2022FRCGameManual.pdf).
